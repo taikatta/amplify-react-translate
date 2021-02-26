97bf42ba5cc1e49b4e71748e4f98d9da908d8eb2
Boglarka Barabas <taikatta@gmail.com>
2021-02-26 11:07:00 +0000

    more languages added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import Amplify from 'aws-amplify';
+import Predictions, { AmazonAIPredictionsProvider } from '@aws-amplify/predictions';
+import awsconfig from './aws-exports';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+Amplify.configure(awsconfig);
+Amplify.addPluggable(new AmazonAIPredictionsProvider());
+
+function TranslateMyText() {
+  const [response, setResponse] = useState("Input some text and click 'Translate'")
+  const [textToTranslate, setTextToTranslate] = useState("");
+  const [targetLang, setTargetLang] = useState('es');
+
+    function translate() {
+      Predictions.convert({
+        translateText: {
+          source: {
+            text: textToTranslate,
+          },
+          targetLanguage: targetLang
+        }
+      }).then(result => setResponse(result.text))
+        .catch(err => setResponse(err.message))
+    }
+
+    function setText(event) {
+      setTextToTranslate(event.target.value);
+    }
+
+    function onChange(event) {
+      setTargetLang(event.target.value);
+    }
+
+    return (
+      <div className="Input">
+        <div style={{ padding: 50 }}>
+          <h3>Text Translation</h3>
+          <input value={textToTranslate} onChange={setText}></input>
+          <button onClick={translate}>Translate</button>
+
+          <h3>Translated text</h3>
+          <p>{response}</p>
+            <select value={targetLang} onChange={onChange}>
+              <option value='es'>Spanish</option>
+              <option value='ar'>Arabic</option>
+              <option value='hu'>Hungarian</option>
+              <option value='zh'>Chinese</option>
+              <option value='el'>Greek</option>
+              <option value='pl'>Polish</option>
+            </select>
+        </div>
+      </div>
+    );
 }
 
-export default App;
+export default TranslateMyText;
